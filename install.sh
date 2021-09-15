cd8719c945f4fad1a081c1c7c3746a076757d11f
CT-66 <css.com@protonmail.com>
2021-09-15 19:12:30 +0530

    15 Sep, 2021 | 07:12 PM

--- install.sh
+++ install.sh
@@ -5,3 +5,15 @@
 if [ -d "$HOME/.config" ]; then
     echo "[test] ~/.config exists!"
 fi
+
+# zshenv symlink
+# echo $zdotdir to .zshenv symlink in .config
+# only nvim, zsh as of now
+# nvim +PlugInstall +qall ; echo "Installed nvim plugins" (if successful, else echo "failed to install nvim plugins")
+# make every existing directory to *.backup
+#
+# if [ $? -eq 0 ]; then
+    # echo OK
+# else
+    # echo FAIL
+# fi
