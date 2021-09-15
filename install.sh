3bc3eac2088c97d9138648350db8929ba57c06f6
CT-66 <css.com@protonmail.com>
2021-09-15 19:19:23 +0530

    15 Sep, 2021 | 07:19 PM

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ fi
 # zshenv symlink
 # echo $zdotdir to .zshenv symlink in .config
 # only nvim, zsh as of now
+# if git is not installed, echo "git is not installed. please install git and re-run this script"
 # install vim plug
 # nvim +PlugInstall +qall ; echo "Installed nvim plugins" (if successful, else echo "failed to install nvim plugins")
 # make every existing directory to *.backup
