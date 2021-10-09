ea97442412a47471375d9e9bfbcf378d26cd1af6
CT-66 <css.com@protonmail.com>
2021-10-10 04:02:25 +0530

    10 Oct, 2021 | 04:02 AM

--- install.sh
+++ install.sh
@@ -2,18 +2,26 @@
 # not completed yet
 # do not use!!
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+
 CONFIG_DIR=$HOME/.config
 
 if [ -d "$HOME/.config" ]; then
     echo "[test] ~/.config exists!"
 fi
 
+# TODO
+# install zsh
+# chsh -s /usr/bin/zsh
 # if .config exists, dont do anything; else, mkdir $HOME/.config
 # if -d $HOME/.config/zsh, dont do anything; else, mkdir CONFIG_DIR/zsh
 # if -d $HOME/.config/nvim, dont do anything; else, mkdir CONFIG_DIR/nvim
 # zshenv symlink
 # echo $zdotdir to .zshenv symlink in .config
-# only nvim, zsh as of now
 # if git is not installed, echo "git is not installed. please install git and re-run this script"
 # install vim plug
 # nvim +PlugInstall +qall ; echo "Installed nvim plugins" (if successful, else echo "failed to install nvim plugins")
@@ -25,3 +33,21 @@ fi
 # else
     # echo FAIL
 # fi
+#
+# sudo pacman -S --needed base-devel
+# git clone https://aur.archlinux.org/paru.git
+# cd paru
+# makepkg -si
+# programs=(list
+#      of
+#      programs
+#      in
+#      a
+#      array
+#  )
+# for program in "${programs[@]}"; do
+#         sudo pacman -S "$program"
+#                 done
+# cd $HOME/.local/share/nvim/plugged/vim-hexokinase
+# make hexokinase
+# cd $HOME
